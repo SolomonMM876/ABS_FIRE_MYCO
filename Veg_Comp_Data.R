b57b5e50f4398e6e07fedcc6f5158603815f22ac
SolomonMM876 <s.maerowitz-mcmahan@westernsydney.edu.au>
2025-03-09 14:11:19 +1100

    last commit

--- Veg_Comp_Data.R
+++ Veg_Comp_Data.R
@@ -17,11 +17,11 @@ Study_sites<-read.csv('Processed_data/All_Bag_data.csv')%>%
 
 ABS_Floristics<- read.csv("Raw_data/Site_Data/ABS_Floristic plot data_ANALYSES_V2_20240819_CEG.cleaned.csv")[,-1]%>%
   mutate(SiteNo = gsub('ABS00|ABS0', "", SiteNo))%>%
-  rename(Site=SiteNo)%>%
-  inner_join(Study_sites, by='Site')
+  rename(Site=SiteNo)#%>%
+  #inner_join(Study_sites, by='Site')
 
 library(APCalign)
-
+#load for austraits
 tax_resources <- load_taxonomic_resources()
 
 spp_list<-ABS_Floristics%>%distinct(ScientificName)
@@ -60,24 +60,46 @@ brundrett_2017 <- read_csv("Processed_data/brundrett_20170327.csv")%>%
   mutate(Family = str_replace(Family, "^(Euphorbaceae|Fabaceae)\\b.*", "\\1"),
         Family = str_replace(Family, "Euphorbaceae", "Euphorbiaceae"))
 
-root_trait_tax <- upd_spp_list %>%
-  # First join: Match trait data based on Genus, some taxa are dual, but we just care if they are or are not
+ 
+# First join: Match trait data based on Genus, some taxa are dual, but we just care if they are or are not
+genus_myco <- upd_spp_list %>%
   left_join(
-    brundrett_2017 %>% rename(Myco_type=Primary.Obs)%>%
-      group_by(Genus) %>%  
-      summarise(Myco_type = paste(unique(Myco_type), collapse = " | "), .groups = "drop"),  # Collapse multiple values, add taxon-based column
-    by = c("genus"="Genus"))%>%
+    brundrett_2017 %>%
+      rename(Myco_type = Primary.Obs) %>%
+      group_by(Genus) %>%
+      summarise(Myco_type = paste(unique(Myco_type), collapse = " | "), .groups = "drop"),  
+    by = c("genus" = "Genus")
+  ) %>%
+  mutate(
+    Myco_type = case_when(
+      genus %in% c("Thysanotus", "Cassytha", "Olax") ~ "NM",
+      genus %in% c('Doryanthes',"Brunoniella",'Caesia') ~ "Unkown",
+      TRUE ~ Myco_type),
+    Notes = case_when(
+      genus == "Thysanotus" ~ "Unique sub-epidermal association - McGee 1988, Brundrett & Abbott 1991.",
+      genus == "Cassytha" ~ "Can have members of family that are mycorrhizal, but all species observed are hemi-parasitic growing on stems and are NM as such.",
+      genus == "Brunoniella" ~ "Type not determined due to lack of records, removed from analysis.",
+      #taxon_name == "Doryanthes excelsa" ~ "Type not determined due to lack of records, classified as NM for analysis.",
+      #taxon_name == "Caesia sp." ~ "Type not determined due to lack of records, classified as NM for analysis.",
+      genus == "Olax" ~ "Non-mycorrhizal Bellgard et al. 1994",
+      is.na(Myco_type) ~ "Classification based on other genera in family",
+      TRUE ~ ""))
+
+family_myco<-genus_myco%>%
   left_join(brundrett_2017%>% rename(Myco_type_fam=Primary.Obs)%>%
                group_by(Family) %>%  
-               summarise(Myco_type_fam = paste(unique(Myco_type_fam), collapse = " | "), .groups = "drop"),
+               summarise(Myco_type_fam = paste(unique(Myco_type_fam), collapse = " | "), .groups = "drop",),
             by= c('family'='Family'))%>%
   mutate(Myco_type= coalesce(Myco_type,Myco_type_fam))%>%
-  mutate(Myco_type = case_when(
-    genus %in% c("Thysanotus","Cassytha","Brunoniella") ~ "NM",  # Assign "NM" if Genus is Thysanotus
-    TRUE ~ Myco_type  ))  # Keep existing values for other genera
-    #Cassytha gen.can have members of fam can be myco, but all species are hemi-parasitic and are NM as such
-    #Thysanotus are a weird case, see Brundrett 2008, but I dont think they are contributing to myco presence
-  
+  select(-Myco_type_fam)
+
+root_trait_tax<-family_myco%>%
+  left_join(genus_myco)
+
+myco_type_pub<-root_trait_tax%>%rename(Species=original_name, Family=family,Genus=genus,Type=Myco_type)%>%
+  select(Genus,Family,Type,Notes)%>%distinct()
+
+write.csv(myco_type_pub,'processed_data/Myco_typ_pub.csv', row.names = FALSE)
 
 unk_myco_plants<-root_trait_tax%>%
   filter(is.na(Myco_type))%>%
@@ -91,6 +113,7 @@ unk_myco_plants<-root_trait_tax%>%
 ABS_Floristics_root_str <- ABS_Floristics %>%
   filter(!ScientificName%in% c('Unknown','unknown'))%>%
   left_join(root_trait_tax, by =c("ScientificName"="original_name"))%>%
+  filter(Myco_Status=='Unkown')%>%
   mutate(Myco_Status = case_when(
     grepl("VAM|ECM|Ericoid|Orchid|MH|MHOrchid|MHVAM", Myco_type) ~ "Myco", 
     grepl("NM", Myco_type) ~ "Non_Myco", 
@@ -130,7 +153,7 @@ Myco__total <- ABS_Floristics_root_str %>%
     Total_Transect2 = sum(Transect2)
   ) %>%
   pivot_wider(names_from = Myco_Status, values_from = c(Total_Transect1, Total_Transect2), values_fill = 0) %>%
-  select(-ends_with("Non_Myco")) %>%
+  select(-ends_with("Non_Myco"),-ends_with("Unknown")) %>%
   pivot_longer(
     cols = starts_with("Total_"),
     names_to = "Transect",
